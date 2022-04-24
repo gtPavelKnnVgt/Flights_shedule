8d2cfef49417822417f48bac4dde21aec8c1fcea
Pavel_KNNV <kononov040801@yandex.ru>
2022-04-25 00:36:20 +0300

    Update DirectionTests class

--- Flights_shedule/Domain.Tests/DirectionTests.cs
+++ Flights_shedule/Domain.Tests/DirectionTests.cs
@@ -13,35 +13,27 @@ namespace Domain.Tests
         [Test]
         public void ValidData_ToString_Success()
         {
-            // arrange
             var direction = GenerateDirection();
 
-            // act
             var result = direction.ToString();
 
-            // assert
-            Assert.AreEqual("Москва-Рязань", result);
+            var expected = "Москва-Рязань";
+
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void Ctor_WrongData_EmptyFrom_Fail()
+        public void Ctor_EmptyFrom_ThrowsArgumentOutOfRangeException()
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => _ = GenerateDirection(string.Empty));
         }
 
         [Test]
-        public void Ctor_WrongData_EmptyTo_Fail()
+        public void Ctor_EmptyTo_ThrowsArgumentOutOfRangeException()
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => _ = GenerateDirection("Пенза", string.Empty));
         }
 
-        [Test]
-
-        public void Ctor_WrongData__EmptyDirection_Fail()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() => _ = GenerateDirection(string.Empty, string.Empty));
-        }
-
-        private static Direction GenerateDirection(string from = null, string to = null) => new (1, from ?? "Москва", to ?? "Рязань");
+        private static Direction GenerateDirection(string from = null, string to = null) => new (id: 1, from ?? "Москва", to ?? "Рязань");
     }
 }
